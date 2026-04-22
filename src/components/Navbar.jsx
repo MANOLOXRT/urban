95fdc0644886faa86c92bfdcd1f4ada1d4742d50
MANOLOXRT <manoloproxrt@gmail.com>
2026-04-22 07:45:50 -0500

    Organizacion y limpieza de estructura

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,1 @@
-import { Link } from 'react-router-dom';
-
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="logo">URBAN THREADS</div>
-      <ul>
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/productos">Productos</Link></li>
-        <li><Link to="/contacto">Contacto</Link></li>
-      </ul>
-    </nav>
-  );
-};
-export default Navbar;
+jjajakdk
---
+++ src/pages/Contacto.jsx
@@ -0,0 +1,1 @@
+jhadjaj
